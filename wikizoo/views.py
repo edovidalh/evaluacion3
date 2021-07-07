a35a07a025b6687ed0c54f454259af89882ce529
edu <evidalhevia@gmail.com>
2021-07-07 07:04:22 -0400

    css+img

--- wikizoo/views.py
+++ wikizoo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'wikizoo/index.html')
+def main(request):
+    return render(request, 'wikizoo/Main.html')
+
+def animal(request):
+    return render(request, 'wikizoo/Animal.html')
