96b1d1c1a69cc0fd281ea23f327e5f0d3f2c2bc4
edu <evidalhevia@gmail.com>
2021-07-07 03:52:38 -0400

    p3

---
+++ wikizoo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WikizooConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wikizoo'
---
+++ wikizoo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class EntradaAnimal(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    nombreAnimal = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    creacion = models.DateTimeField(
+        default=timezone.now)
+    publicacion = models.DateTimeField(
+        blank=True, null=True)
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
